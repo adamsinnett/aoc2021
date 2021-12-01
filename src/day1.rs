bfaf83bbe2d808220c3296cc93c73434218da180
Adam Sinnett <quandrum@gmail.com>
2021-11-30 22:00:08 -0800

    [Day1] Use windows and slices to improve code quality

--- src/day1.rs
+++ src/day1.rs
@@ -5,36 +5,20 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day1, part1)]
 pub fn part1(input: &Vec<i32>) -> usize {
-    let mut clone = input.clone();
-    clone.remove(0);
-
-    let zip = input.iter().zip(clone.iter());
-
-    zip.filter(|(a, b)| a < b).count()
+    input
+        .iter()
+        .zip(input[1..].iter())
+        .filter(|(a, b)| a < b)
+        .count()
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &Vec<i32>) -> usize {
-    let mut first = input.clone();
-    first.remove(0);
-    first.remove(1);
-
-    let mut second = input.clone();
-    second.remove(0);
-
-    let window: Vec<i32> = second
-        .iter()
-        .zip(first.iter())
-        .map(|(a, b)| a + b)
-        .zip(input.iter())
-        .map(|(a, b)| a + b)
-        .collect();
-
-    part1(&window)
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn sample1() {}
+    part1(
+        &(input
+            .windows(3)
+            .into_iter()
+            .map(|x| x.iter().sum())
+            .collect()),
+    )
 }
