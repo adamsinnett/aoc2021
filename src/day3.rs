6ea858acdc607251ead684cbda2316b42ac983ad
Adam Sinnett <quandrum@gmail.com>
2021-12-03 08:54:01 -0800

    [Day3] Use fn ref to deduplicate

--- src/day3.rs
+++ src/day3.rs
@@ -26,18 +26,21 @@ pub fn part1(input: &str) -> i32 {
 
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> i32 {
-    let sample =
-        "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010";
     let lines: Vec<&str> = input.lines().collect();
-    let mut freqs: Vec<i32> = vec![0; lines[0].len()];
 
-    let oxygen = i32::from_str_radix(filter_oxygen(lines.clone(), 0)[0], 2).unwrap();
-    let co2 = i32::from_str_radix(filter_co2(lines.clone(), 0)[0], 2).unwrap();
+    fn oxygen_comp(freq: i32) -> bool {
+        freq >= 0
+    }
+    fn co2_comp(freq: i32) -> bool {
+        freq < 0
+    }
+    let oxygen = i32::from_str_radix(find(lines.clone(), 0, oxygen_comp)[0], 2).unwrap();
+    let co2 = i32::from_str_radix(find(lines.clone(), 0, co2_comp)[0], 2).unwrap();
 
     oxygen * co2
 }
 
-fn filter_oxygen<'a>(lines: Vec<&'a str>, idx: usize) -> Vec<&'a str> {
+fn find<'a>(lines: Vec<&'a str>, idx: usize, comp: fn(i32) -> bool) -> Vec<&'a str> {
     if lines.len() == 1 {
         lines.iter().cloned().collect()
     } else {
@@ -49,58 +52,25 @@ fn filter_oxygen<'a>(lines: Vec<&'a str>, idx: usize) -> Vec<&'a str> {
             }
         });
 
-        if freq >= 0 {
-            filter_oxygen(
+        if comp(freq) {
+            find(
                 lines
                     .iter()
                     .filter(|&line| line.bytes().nth(idx) == Some(b'1'))
                     .cloned()
                     .collect(),
                 idx + 1,
+                comp,
             )
         } else {
-            filter_oxygen(
+            find(
                 lines
                     .iter()
                     .filter(|&line| line.bytes().nth(idx) == Some(b'0'))
                     .cloned()
                     .collect(),
                 idx + 1,
-            )
-        }
-    }
-}
-
-// This is ugly and the comparison should be dynamic
-fn filter_co2<'a>(lines: Vec<&'a str>, idx: usize) -> Vec<&'a str> {
-    if lines.len() == 1 {
-        lines.iter().cloned().collect()
-    } else {
-        let freq = lines.iter().fold(0, |accum, line| {
-            if line.bytes().nth(idx) == Some(b'1') {
-                accum + 1
-            } else {
-                accum - 1
-            }
-        });
-
-        if freq < 0 {
-            filter_co2(
-                lines
-                    .iter()
-                    .filter(|&line| line.bytes().nth(idx) == Some(b'1'))
-                    .cloned()
-                    .collect(),
-                idx + 1,
-            )
-        } else {
-            filter_co2(
-                lines
-                    .iter()
-                    .filter(|&line| line.bytes().nth(idx) == Some(b'0'))
-                    .cloned()
-                    .collect(),
-                idx + 1,
+                comp,
             )
         }
     }
