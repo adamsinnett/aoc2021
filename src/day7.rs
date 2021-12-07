365714328c50921f3b0beeff1a04ba108caf8983
Adam Sinnett <quandrum@gmail.com>
2021-12-06 22:07:46 -0800

    [Day 7] Cleanup

--- src/day7.rs
+++ src/day7.rs
@@ -1,34 +1,28 @@
-use std::cmp::{max, min};
-
 #[aoc_generator(day7)]
-pub fn input_generator(input: &str) -> Vec<usize> {
+pub fn input_generator(input: &str) -> Vec<i64> {
     input
         .split(",")
-        .map(|d| d.parse::<usize>().unwrap())
+        .map(|d| d.parse::<i64>().unwrap())
         .collect()
 }
 
 #[aoc(day7, part1)]
-pub fn part1(input: &Vec<usize>) -> usize {
-    let middle = input.clone().iter().count() / 2;
+pub fn part1(input: &Vec<i64>) -> i64 {
+    let median = input.clone().iter().count() / 2;
 
     let mut sorted = input.clone();
     sorted.sort();
-    let median = sorted.get(middle).unwrap();
-    input.iter().map(|d| max(d, median) - min(d, median)).sum()
+    let n = sorted.get(median).unwrap();
+    input.iter().map(|d| (d - n).abs()).sum()
 }
 
 #[aoc(day7, part2)]
-pub fn part2(input: &Vec<usize>) -> i64 {
-    let mean: f64 = input.clone().iter().map(|d| *d as i32).sum::<i32>() as f64 / input.len() as f64;
-    let rounded = mean as i64;
+pub fn part2(input: &Vec<i64>) -> i64 {
+    let mean: f64 = input.clone().iter().sum::<i64>() as f64 / input.len() as f64;
 
     input
         .iter()
-        .map(|d| *d as i64)
-        .map(|d| (d - rounded).abs())
-        .map(|n| { 
-            n * (n + 1) / 2
-        })
-        .sum::<i64>()
+        .map(|d| (d - mean as i64).abs())
+        .map(|n| n * (n + 1) / 2)
+        .sum()
 }
